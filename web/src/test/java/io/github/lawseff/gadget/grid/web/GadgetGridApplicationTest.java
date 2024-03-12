bd0fa95bd5f1cae41f9fdc7fb0902e6e445334f1
Mikhail Loseu <mikhail.loseu@gmail.com>
2024-03-12 20:31:41 +0300

    GG-5: Create base project
    The app is planned to have a list of gadgets, that are stored in a DB
    and can be read via the REST API. The commit adds a Maven project with modules,
    that should be used to impelement that

---
+++ web/src/test/java/io/github/lawseff/gadget/grid/web/GadgetGridApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.lawseff.gadget.grid.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetGridApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
